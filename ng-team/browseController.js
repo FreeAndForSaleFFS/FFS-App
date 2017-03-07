35b5b4ffb531d2a531bf1834d08438454c27260f
FreeAndForSaleFFS <FreeAndForSaleFFS@gmail.com>
2017-03-07 08:38:43 -0800

    commit #16
    Average Price Feature

--- ng-team/browseController.js
+++ ng-team/browseController.js
@@ -6,7 +6,7 @@ angular
     'use strict';
     //maximum amount of posts per page at page start.
     var postsLimit = 5;
-
+    $scope.averageValue = 0;
     var urlCheck = document.location.href;
     if(urlCheck.includes("?")) {
        var url = document.location.href,
@@ -30,6 +30,7 @@ angular
           angular.forEach($scope.requestData, function (requestData) {
               requestData.itemPrice = parseFloat(requestData.itemPrice);
           });
+          $scope.computeAverage();
       });
     });
     
@@ -43,6 +44,7 @@ angular
                 angular.forEach($scope.requestData, function (requestData) {
                     requestData.itemPrice = parseFloat(requestData.itemPrice);
                 });
+                $scope.computeAverage();
             });
         });
     };
@@ -56,6 +58,7 @@ angular
                 angular.forEach($scope.requestData, function (requestData) {
                     requestData.itemPrice = parseFloat(requestData.itemPrice);
                 });
+                $scope.computeAverage();
             });
         });
     };
@@ -66,4 +69,27 @@ angular
         $scope.limit += postsLimit;
         console.log(postsLimit);
     };
+    
+    $scope.computeAverage = function() {
+        console.log("check");
+        var count = 0;
+        var average = $scope.averageValue;
+        var searchText = $scope.searchBy;
+        if(!searchText) {
+            searchText = "";
+        }
+        searchText = searchText.toUpperCase();
+        console.log(searchText);
+        angular.forEach($scope.requestData, function (requestData) {
+            var dataName = requestData.itemName;
+            dataName = dataName.toUpperCase();
+            if(dataName.includes(searchText)) {
+                  console.log(requestData.itemName);
+                  average = (count*average + requestData.itemPrice)/(count + 1);
+                  count = count+1;
+              }
+          });
+        console.log(average);
+        $scope.averageValue = average;
+    }
 });
