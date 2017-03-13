41ba5daf7d6a9b50f08268ddbeb5bf51c6a0bec0
Eric Syu <ejsyu@ucsd.edu>
2017-03-12 19:21:43 -0700

    price trend

--- ng-team/pricetrendController.js
+++ ng-team/pricetrendController.js
@@ -145,23 +145,111 @@ angular
             $scope.averageValue = priceRet.toString();
             temp = priceRet;
 
-            var dataObj={
-                type:"line",
-                title:{
-                    text:"Chart Data Object"
+            var myConfig = 
+            {
+                "type": "line",
+                "utc": true,
+                "title": {
+                    "text": "Webpage Analytics",
+                    "font-size": "24px",
+                    "adjust-layout":true
                 },
-                series:[
+                "plotarea": {
+                    "margin": "dynamic 45 60 dynamic",
+                },
+                "legend": {
+                    "layout": "float",
+                    "background-color": "none",
+                    "border-width": 0,
+                    "shadow": 0,
+                    "align":"center",
+                    "adjust-layout":true,
+                    "item":{
+                      "padding": 7,
+                      "marginRight": 17,
+                      "cursor":"hand"
+                    }
+                },
+                "scale-x": {
+                    "min-value": 1383292800000,
+                    "shadow": 0,
+                    "step": 3600000,
+                    "transform": {
+                        "type": "date",
+                        "all": "%D, %d %M<br />%h:%i %A",
+                        "guide": {
+                            "visible": false
+                        },
+                        "item": {
+                            "visible": false
+                        }
+                    },
+                    "label": {
+                        "visible": false
+                    },
+                    "minor-ticks": 0
+                },
+                "scale-y": {
+                    "auto-fit": "true",
+                    "line-color": "#f6f7f8",
+                    "shadow": 0,
+                    "guide": {
+                        "line-style": "dashed"
+                    },
+                    "label": {
+                        "text": "Page Views",
+                    },
+                    "minor-ticks": 0,
+                    "thousands-separator": ","
+                },
+                "crosshair-x": {
+                    "line-color": "#efefef",
+                    "plot-label": {
+                        "border-radius": "5px",
+                        "border-width": "1px",
+                        "border-color": "#f6f7f8",
+                        "padding": "10px",
+                        "font-weight": "bold"
+                    },
+                    "scale-label": {
+                        "font-color": "#000",
+                        "background-color": "#f6f7f8",
+                        "border-radius": "5px"
+                    }
+                },
+                "tooltip": {
+                    "visible": false
+                },
+                "plot": {
+                    "highlight":true,
+                    "tooltip-text": "%t views: %v<br>%k",
+                    "shadow": 0,
+                    "line-width": "2px",
+                    "marker": {
+                        "type": "circle",
+                        "size": 3
+                    },
+                    "highlight-state": {
+                        "line-width":3
+                    },
+                    "animation":{
+                      "effect":1,
+                      "sequence":2,
+                      "speed":100,
+                    }
+                },
+                "series": [
                     {
-                        values:priceRet
+                        "values": priceRet
                     }
                 ]
             };
-
+ 
             zingchart.render({ 
                 id : 'myChart', 
-                data : dataObj, 
-                height: '80%', 
-                width: '80%' 
+                data : myConfig, 
+                height: '100%', 
+                width: '100%' 
             });
         };
     });
